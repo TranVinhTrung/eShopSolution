9745c0a928c11c5714c06112c10b68436349fe0c
DESKTOP-8TQRG61\Vinh Trung <vinhtrung5555>
2023-01-21 18:54:18 +0700

    Initial migration

---
+++ eShopSolution.Data/EF/EShopDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+namespace eShopSolution.Data.EF
+{
+    public class EShopDbContextFactory : IDesignTimeDbContextFactory<EShopDbContext>
+    {
+        public EShopDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("eShopSolutionDb");
+
+            var optionsBulder = new DbContextOptionsBuilder<EShopDbContext>();
+            optionsBulder.UseSqlServer(connectionString);
+
+            return new EShopDbContext(optionsBulder.Options);
+        }
+    }
+}
